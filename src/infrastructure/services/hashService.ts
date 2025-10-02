c26e7006d11c8f258ffcc09cf8b728459c36f75c
Alfredo <baptistaalfredo81@gmail.com>
2025-10-02 18:48:13 +0100

    refotor: injeção da variavel salt_round

--- src/infrastructure/services/hashService.ts
+++ src/infrastructure/services/hashService.ts
@@ -2,17 +2,13 @@ import bcrypt from 'bcrypt';
 import { IHashService } from '../../application/services/hashService';
 
 export class HashService implements IHashService {
-
-    constructor (private readonly saltRounds: number) {}
+    constructor(private readonly saltRounds: number) {}
 
     public async hash(data: string): Promise<string> {
         return bcrypt.hash(data, this.saltRounds);
     }
-    
-    public async compare(
-        data: string, 
-        hashed: string
-    ): Promise<boolean> {
+
+    public async compare(data: string, hashed: string): Promise<boolean> {
         return bcrypt.compare(data, hashed);
     }
 }
