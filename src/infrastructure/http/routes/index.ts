8b8868b9b8a3598f98aece9236f218339d03e40b
Alfredo <baptistaalfredo81@gmail.com>
2025-10-02 18:50:04 +0100

    refactor: remoção do errorHandler global

--- src/infrastructure/http/routes/index.ts
+++ src/infrastructure/http/routes/index.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express';
 import { movimentoRouter } from './MovimentoRoute';
 import { userRouter } from './UserRoute';
-import { erroHandler } from '../middlewares/errorMiddleware';
 
 export const routeIndex = Router();
 routeIndex.use('/auth', userRouter);
 routeIndex.use('/movimentos', movimentoRouter);
-routeIndex.use(erroHandler);
